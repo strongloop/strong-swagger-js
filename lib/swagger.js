strongloop/strong-swagger-js

commit 8a3cd623c2de6ab85bc8394bdeb98b44a8070627

Ported changes from SwaggerUI for handling of arrays of types/models, defaultValues, basePaths, and large descriptions

lib/swagger.js
@@ -450,6 +450,9 @@
       this.consumes = response.consumes;
     if ((response.basePath != null) && response.basePath.replace(/\s/g, '').length > 0)
       this.basePath = response.basePath.indexOf('http') === -1 ? this.getAbsoluteBasePath(response.basePath) : response.basePath;
+    if (response.basePaths != null) {
+      this.basePaths = response.basePaths;
+    }
 
     this.addModels(response.models);
     if (response.apis) {
@@ -631,25 +634,28 @@
         this.refDataType = obj.items.$ref;
       }
     }
-    this.dataTypeWithRef = this.refDataType != null ? (this.dataType + '[' + this.refDataType + ']') : this.dataType;
+    this.dataTypeWithRef = this.refDataType != null ? (this.dataType[0].toUpperCase() + this.dataType.substring(1, this.dataType.length) + ' of ' + this.refDataType) : this.dataType;
     if (obj.allowableValues != null) {
       this.valueType = obj.allowableValues.valueType;
       this.values = obj.allowableValues.values;
       if (this.values != null) {
-        this.valuesString = '\'' + this.values.join('\' or \'') + '\'';
+        this.valuesString = "'" + this.values.join("', '") + "'";
       }
     }
     if (obj['enum'] != null) {
       this.valueType = 'string';
       this.values = obj['enum'];
       if (this.values != null) {
-        this.valueString = '\'' + this.values.join('\' or \'') + '\'';
+        this.valueString = "'" + this.values.join("', '") + "'";
       }
     }
   }
 
   SwaggerModelProperty.prototype.getSampleValue = function (modelsToIgnore) {
     var result;
+    if (this.defaultValue) {
+      return this.defaultValue;
+    }
     if ((this.refModel != null) && (modelsToIgnore.indexOf(this.refModel.name) === -1)) {
       result = this.refModel.createJSONSample(modelsToIgnore);
     } else {
@@ -670,14 +676,14 @@
     var result;
     if ((typeof this.defaultValue !== 'undefined') && this.defaultValue !== null) {
       result = this.defaultValue;
-    } else if (value === 'integer') {
-      result = 0;
-    } else if (value === 'boolean') {
-      result = false;
-    } else if (value === 'double' || value === 'number') {
-      result = 0.0;
-    } else if (value === 'string') {
-      result = '';
+    } else if (value === "integer") {
+      result = 1;
+    } else if (value === "boolean") {
+      result = true;
+    } else if (value === "double" || value === "number") {
+      result = 1.1;
+    } else if (value === "string") {
+      result = ' ';
     } else {
       result = value;
     }
@@ -692,10 +698,11 @@
     }
     str += ')';
     if (this.values != null) {
-      str += ' = <span class="propVals">["' + this.values.join('\' or \'') + '\']</span>';
+      str += " = <span class='propVals'>['" + this.values.join("', '") + "']</span>";
     }
     if (this.descr != null) {
-      str += ': <span class="propDesc">' + this.descr + '</span>';
+      var descr = Array.isArray(this.descr) ? this.descr.join(' ') : this.descr;
+      str += ': <span class="propDesc">' + descr + '</span>';
     }
     return str;
   };
@@ -732,8 +739,9 @@
     if (typeof this.type !== 'undefined' && this.type === 'void')
       this.type = null;
     else {
-      this.responseClassSignature = this.getSignature(this.type, this.resource.models);
-      this.responseSampleJSON = this.getSampleJSON(this.type, this.resource.models);
+      var _this = this;
+      this.responseClassSignature = function () { return _this.getSignature(_this.type, _this.resource.models) }
+      this.responseSampleJSON = function () { return _this.getSampleJSON(_this.type, _this.resource.models) }
     }
 
     for (var i = 0; i < this.parameters.length; i++) {
@@ -752,8 +760,9 @@
         param.allowableValues = {};
         param.allowableValues.values = ['true', 'false'];
       }
-      param.signature = this.getSignature(type, this.resource.models);
-      param.sampleJSON = this.getSampleJSON(type, this.resource.models);
+      var _this = this;
+      param.signature = function (param) { return _this.getSignature(param.type || param.dataType, _this.resource.models) };
+      param.sampleJSON = function (param) { return _this.getSampleJSON(param.type || param.dataType, _this.resource.models) };
 
       var enumValue = param['enum'];
       if (enumValue != null) {
@@ -841,10 +850,14 @@
     listType = this.isListType(type);
     isPrimitive = ((listType != null) && models[listType]) || (models[type] != null) ? false : true;
     if (isPrimitive) {
-      return type;
+      if (listType) {
+        return '<span class="strong">Array of </span>' + listType;
+      } else {
+        return type;
+      }
     } else {
-      if (listType != null) {
-        return models[listType].getMockSignature();
+      if (listType) {
+        return '<span class="strong">Array of </span>' + models[listType].getMockSignature();
       } else {
         return models[type].getMockSignature();
       }
@@ -855,12 +868,19 @@
     var isPrimitive, listType, val;
     listType = this.isListType(type);
     isPrimitive = ((listType != null) && models[listType]) || (models[type] != null) ? false : true;
-    val = isPrimitive ? void 0 : (listType != null ? models[listType].createJSONSample() : models[type].createJSONSample());
+    val = null;
+    if (isPrimitive) {
+      val = SwaggerModelProperty.prototype.toSampleValue(listType || type);
+    } else {
+      val = models[listType || type].createJSONSample();
+    }
     if (val) {
-      val = listType ? [val] : val;
-      if (typeof val == 'string')
-        return val;
-      else if (typeof val === 'object') {
+      if (typeof val === "string") {
+        val = '"' + val + '"';
+      }
+      if (listType) {
+        return '[' + val + ']';
+      } else if (typeof val === "object") {
         var t = val;
         if (val instanceof Array && val.length > 0) {
           t = val[0];
@@ -1007,16 +1027,16 @@
       var param = params[i];
       if(param.paramType === 'query') {
         if (queryParams !== '')
-          queryParams += '&';    
+          queryParams += '&';
         if (Array.isArray(param)) {
-          var j;   
-          var output = '';   
-          for(j = 0; j < param.length; j++) {    
-            if(j > 0)    
-              output += ',';   
-            output += encodeURIComponent(param[j]);    
-          }    
-          queryParams += encodeURIComponent(param.name) + '=' + output;    
+          var j;
+          var output = '';
+          for(j = 0; j < param.length; j++) {
+            if(j > 0)
+              output += ',';
+            output += encodeURIComponent(param[j]);
+          }
+          queryParams += encodeURIComponent(param.name) + '=' + output;
         }
         else {
           if (typeof args[param.name] !== 'undefined') {
@@ -1082,7 +1102,7 @@
     var results = [];
     var i;
 
-    var headers = SwaggerRequest.prototype.setHeaders(args, {}, this);    
+    var headers = SwaggerRequest.prototype.setHeaders(args, {}, this);
     for(i = 0; i < this.parameters.length; i++) {
       var param = this.parameters[i];
       if(param.paramType && param.paramType === 'header' && args[param.name]) {

test/modelSignatures.js
@@ -21,13 +21,14 @@ describe('model signatures', function() {
     test.object(pet);
 
     // verify that each instance of `Pet` is represented
-    var petModel = '{"id":0,"category":{"id":0,"name":""},"name":"","photoUrls":[""],"tags":[{"id":0,"name":""}],"status":"","phone":{"code":"","number":""},"owner":{"name":"","phone":{"code":"","number":""}}}';
+    var petModel = '{"id":1,"category":{"id":1,"name":" "},"name":" ","photoUrls":[" "],"tags":[{"id":1,"name":" "}],"status":" ","phone":{"code":" ","number":" "},"owner":{"name":" ","phone":{"code":" ","number":" "}}}';
     var model = '';
 
     model += '{' + '"cat":' + petModel + ',';
     model += '"dog":' + petModel + ',';
     model += '"mouse":' + petModel + '}';
 
-    should(JSON.stringify(pet.createJSONSample())).equal(model);
-  })  
+    should(JSON.stringify(JSON.parse(model), null, 2))
+      .equal(JSON.stringify(pet.createJSONSample(), null, 2));
+  })
 })

commit 1fd3f859572b32727b7a3f5b519aceeccf149b73

Corrected getSampleJSON() to handle Array of Model
Fix bug introduced in 8a3cd623

Original commit:
  https://github.com/shelbys/swagger-js/commit/19c2e3245

lib/swagger.js
@@ -879,8 +879,10 @@
         val = '"' + val + '"';
       }
       if (listType) {
-        return '[' + val + ']';
-      } else if (typeof val === "object") {
+        val = [val];
+      }
+
+      if (typeof val === "object") {
         var t = val;
         if (val instanceof Array && val.length > 0) {
           t = val[0];